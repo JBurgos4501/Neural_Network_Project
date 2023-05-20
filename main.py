b7ad6153e8b6346c4d006fc52a432a9652f71896
Jose Burgos <j.burgos.pr01@gmail.com>
2023-05-20 14:13:43 -0400

    Testing created model mlp

--- main.py
+++ main.py
@@ -42,9 +42,23 @@ import os
 import pandas as pd
 import numpy as np
 import random
+import time
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import cohen_kappa_score
+from sklearn.model_selection import GridSearchCV
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+#from scikeras.wrappers import KerasClassifier
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+
 
 
 
@@ -66,37 +80,23 @@ def main():
     # join files if needed
     if run_join_files:
         make_joined_files()
-
+        
+    # group files in groups of 4 patients
+    groups=group_patients()
+    
+    train_test_list= get_train_test(groups)
+    
     if get_mlp:
-        model = MLPClassifier(hidden_layer_sizes=(64, 64), activation='relu', solver='adam', max_iter=100, verbose=0)
+        model = create_mlp()
     elif get_hybrid:
         #define CNN or a combination of CNN with RNN
         pass
     elif get_assigned:
         pass
-
-    
-    # group files in groups of 4 patients
-    groups=group_patients()
-    train_test_list= get_train_test(groups)
-
     
     
-    run_model(train_test_list, model)
-    # STEPS:
-    # Iterate through groups using for loop
-    #for group in groups:
-        # Each value in for loop is a list of 4 files, one should be the test patient
-
-        # This has already been done in the group_patients function, if there is any need to change, change there
-        # Make function to determine which are test and which are train (this can be changed to a custom way of splitting later)
-        # Get train as a list of 3 files
-        # Get test as a list of 1 file
-        
-        # Make function that runs the model, the model being passed as a parameter'
-        # Define model object
-        # Read in all data files into dataframe
-        # Check using breakpoint()
+    results_df = run_model(train_test_list, model)
+    print (results_df)
 
 
 def make_joined_files(): 
@@ -171,11 +171,13 @@ def get_train_test(groups):
         # type of variable expected for group[1], string that represents file path
         for file in group[0]:
             train_test_list[0].append(file)
-        
         train_test_list[1].append(group[1])
+
     return train_test_list
 
 def run_model(train_test_list, model, folds=3):
+    label_encoder = LabelEncoder()
+
     train_data = [] # List of dataframes    
     # Load data first
     for train_patient_path in train_test_list[0]: 
@@ -191,8 +193,10 @@ def run_model(train_test_list, model, folds=3):
     train_data = train_data.to_numpy()
 
     kfold = KFold(n_splits=folds, shuffle=True)
-
+    # make dataframe with columns for results per fold
+    results_df = pd.DataFrame(columns=["Fold", "Accuracy","Kappa", "F1-Weighted_Score", "Confusion_Matrix", "Train_Time"])
     for fold, (train_idx, val_idx) in enumerate(kfold.split(train_data)):
+        
         print(f"Fold {fold + 1}")
         # Get the training and validation data
         model_train_data = train_data[train_idx]
@@ -203,69 +207,135 @@ def run_model(train_test_list, model, folds=3):
         y_train = model_train_data[:, -1]
         X_val = model_val_data[:, :-1]
         y_val = model_val_data[:, -1]
+        
+        #convert labels to integers
+        y_train = label_encoder.fit_transform(y_train)
+        y_val = label_encoder.fit_transform(y_val)
 
+        # #convert labels to one hot encoding
+        # onehot_encoder = OneHotEncoder(sparse=False)
+        # y_train = onehot_encoder.fit_transform(y_train)
+        # y_train = onehot_encoder.fit_transform(y_train)
+
+
+        #get train time
+        start_train_time = time.time()
         # Fit the model
         model.fit(X_train, y_train)
+        end_train_time = time.time()
+        
+        # make dictionary to store results, it will be appended to the results_df
+        results_dict = {}
+
+        train_time = end_train_time - start_train_time
+        results_dict["Train_Time"] = train_time
 
         # Predict the labels for the val data
         y_pred = model.predict(X_val)
 
+        # # Get scores
+        # accuracy = accuracy_score(y_val, y_pred)
+        # results_dict["Accuracy"] = accuracy
+
+        # # Get kappa
+        # kappa = cohen_kappa_score(y_val, y_pred)
+        # results_dict["Kappa"] = kappa
         
+        # # Get F1 score
+        # f1 = f1_score(y_val, y_pred, average="weighted")
+        # results_dict["F1-Weighted_Score"] = f1
 
+        # # Get confusion matrix
+        # cm = confusion_matrix(y_val, y_pred)
+        # results_dict["Confusion_Matrix"] = cm
 
-        # # Concatenate the train data and labels
-        # for i in range(len(train_data)):
-        #     train_data[i] = np.concatenate((train_data[i], train_label[i].values.reshape(-1, 1)), axis=1)
+        # # Add fold number
+        # results_dict["Fold"] = fold + 1
 
-        # # define the neural network model
-        # #model = MLPClassifier(hidden_layer_sizes=(64, 64), activation='relu', solver='adam', max_iter=100, verbose=0)
+        # # Append results to results_df
+        # # Turn dictionary into dataframe
+        # results_dict_df = pd.DataFrame.from_dict(results_dict, orient="index").T
 
-        # # Define the K-Fold cross-validator (folds=3)
-        # # Kfold rturns linst of indexes for train and test of a dataset
-        # kfold = KFold(n_splits=folds, shuffle=True)
+        # # Use pd.concat to add the results_df to a results_df
+        # results_df = pd.concat([results_df, results_dict_df], axis=0)
 
-        
-        # # Iterate through the folds and fit the model
-        # # iterate through each train patient
-        # for patient_data in train_data:
-        #     for fold, (train_idx, val_idx) in enumerate(kfold.split(patient_data)):
-        #         print(f"Fold {fold + 1}")
-        #         breakpoint()
-        #         # Get the training and validation data
-        #         for i in range(len(train_data)):
-        #             X_train = train_data[i][train_idx]
-        #             X_val = train_data[i][val_idx]
-        #             y_train = train_label[i][train_idx]
-        #             y_val = train_label[i][val_idx]
+    print("Test")
+    test_data = [] # List of dataframes    
+    # Load data first
+    for test_patient_path in train_test_list[1]: 
 
-        #         # Fit the model
-        #         for i in range(len(train_data)):
-        #             model.fit(X_train, y_train)
-        #         # Predict the labels for the test data
-        #         y_pred = model.predict(test_data) 
+        # Load patient data
+        patient_test_data = pd.read_csv(test_patient_path)
+        test_data.append(patient_test_data)
 
-        #         # Generate the confusion matrix
-        #         cm = confusion_matrix(test_label, y_pred)
-        #         print(cm)
+    # Concatenate the train data
+    test_data = pd.concat(test_data, axis=0)
 
-        #         # Add the current fold's confusion matrix to the total confusion matrix
-        #         cm_total += cm
+    # Convert the train data to a numpy array
+    test_data = test_data.to_numpy()
 
-        #         # Evaluate the model on the test data and print the accuracy
-        #         accuracy = model.score(test_data, test_label)
-        #         print(f"Test Accuracy: {accuracy}\n")
+    # Split the test data into X and y
+    X_test = test_data[:, :-1]
+    y_test = test_data[:, -1]
 
-        # # Print the total confusion matrix after all folds
-        # print(f"Total Confusion Matrix:\n{cm_total}\n")
+    #convert labels to integers
+    #y_test = label_encoder.fit_transform(y_test)
 
-        # # Print results
-        # print("Results:\n")
-        # print("Accuracy: ", np.trace(cm_total) / np.sum(cm_total))
-        # print("Precision: ", np.trace(cm_total) / np.sum(cm_total, axis=0))
-        # print("Recall: ", np.trace(cm_total) / np.sum(cm_total, axis=1))
-        # print("F1: ", 2 * np.trace(cm_total) / (np.sum(cm_total, axis=0) + np.sum(cm_total, axis=1)))
-        # print("\n")
+    # Predict the labels for the test data
+    y_pred = model.predict(X_test)
 
+    # make dictionary to store results, it will be appended to the results_df
+    results_dict = {}
+
+    # # Get scores
+    # accuracy = accuracy_score(y_test, y_pred)
+    # results_dict["Accuracy"] = accuracy
+
+    # # Get kappa
+    # kappa = cohen_kappa_score(y_test, y_pred)
+    # results_dict["Kappa"] = kappa
+    
+    # # Get F1 score
+    # f1 = f1_score(y_test, y_pred, average="weighted")
+    # results_dict["F1-Weighted_Score"] = f1
+
+    # # Get confusion matrix
+    # cm = confusion_matrix(y_test, y_pred)
+    # results_dict["Confusion_Matrix"] = cm
+
+    # # Fold is -1 for test
+    # results_dict["Fold"] = -1
+
+    # # train time is -1 for test
+    # results_dict["Train_Time"] = -1
+
+    # # Append results to results_df
+    # # Turn dictionary into dataframe
+    # results_dict_df = pd.DataFrame.from_dict(results_dict, orient="index").T
+
+    # # Use pd.concat to add the results_df to a results_df
+    # results_df = pd.concat([results_df, results_dict_df], axis=0)
+
+    return results_df
+
+def create_mlp():
+    model = Sequential()
+   
+    # Add the input layer
+    model.add(Dense(units=64, activation='relu', input_dim=3000))
+    
+    # Add one or more hidden layers
+    model.add(Dense(units=64, activation='relu'))
+    model.add(Dense(units=64, activation='relu'))
+    
+    # Add the output layer
+    model.add(Dense(units=5, activation='softmax'))
+
+    # Compile the model
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    
+    return model
 
 
 main()
+
